93bd80e9cdc4cfe837f0cca7e9a0f0fa7661048d
John Maruska <john.maruska@rate.com>
2020-07-15 17:05:02 -0500

    can run locally - didn't verify behavior

--- README.md
+++ README.md
@@ -12,14 +12,23 @@ T.N.T. (Toil 'n Trouble) is a D&D 5e character sheet application. Please do not 
 
 FIXME: Write a paragraph about the library/project and highlight its goals.
 
+
 ## Setup
 
-To get an interactive development environment run:
+Install all dependencies
 
+    lein install
     npm install
+
+Run the back-end server
+
+    lein do clean, run
+
+Run the front-end server
+
     lein figwheel
 
-and open your browser at [localhost:3449](http://localhost:3449/).
+Figwheel should automatically open default browser at [localhost:3449](http://localhost:3449).
 This will auto compile and send all changes to the browser without the
 need to reload. After the compilation process is complete, you will
 get a Browser Connected REPL. An easy way to try it is:
@@ -28,19 +37,8 @@ get a Browser Connected REPL. An easy way to try it is:
 
 and you should see an alert in the browser window.
 
-To clean all compiled files:
-
-    lein clean
-
-To create a production build run:
-
-    lein do clean, cljsbuild once min
-
-And open your browser in `resources/public/index.html`. You will not
-get live reloading, nor a REPL. 
-
 ## License
 
-Copyright © 2014 FIXME
+Copyright © 2020 FIXME
 
 Distributed under the Eclipse Public License either version 1.0 or (at your option) any later version.
--- project.clj
+++ project.clj
@@ -1,17 +1,15 @@
 (defproject tnt "0.1.0-SNAPSHOT"
-  :description "FIXME: write this!"
-  :url "http://example.com/FIXME"
+  :description "Toil & Trouble - Dungeons & Dragons character sheets and more."
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
   :min-lein-version "2.9.1"
 
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.773"]
-                 ;; [org.clojure/core.async  "0.4.500"]
                  [hiccup "1.0.5"]
                  [reagent "1.0.0-alpha2"]
-                 [metosin/reitit "0.5.1"]
+                 [metosin/reitit "0.5.5"]
                  [ring "1.8.1"]]
 
   :plugins [[lein-figwheel "0.5.20"]
@@ -25,8 +23,6 @@
                 :source-paths ["src/cljs"]
 
                 :figwheel {:on-jsload "tnt.core/on-js-reload"
-                           ;; pop open your application in the default browser
-                           ;; once Figwheel has started your compiled application.
                            :open-urls ["http://localhost:3449/index.html"]}
 
                 :compiler {:main                 tnt.core
@@ -34,27 +30,13 @@
                            :asset-path           "js/compiled/out"
                            :output-to            "resources/public/js/compiled/out/index.js"
                            :output-dir           "resources/public/js/compiled/out"
-                           :bundle-cmd           {:none    ["npx.cmd" "webpack" "--mode=development"]
-                                                  :default ["npx.cmd" "webpack"]}
                            :source-map-timestamp true
                            ;; To console.log CLJS data-structures make sure you enable devtools in Chrome
                            ;; https://github.com/binaryage/cljs-devtools
-                           :preloads             [devtools.preload]}}
-               ;; This next build is a compressed minified build for
-               ;; production. You can build this with:
-               ;; lein cljsbuild once min
-               {:id           "min"
-                :source-paths ["src/cljs"]
-                :compiler     {:output-to     "resources/public/js/compiled/tnt.js"
-                               :main          tnt.core
-                               :optimizations :advanced
-                               :pretty-print  false}}]}
+                           :preloads             [devtools.preload]}}]}
 
-  :figwheel {:http-server-root "public"
-             :ring-handler     tnt.handler/app
-             :server-port      3449
-             ;; :server-ip "127.0.0.1"
-             :css-dirs         ["resources/public/css"]}
+  :figwheel {:ring-handler tnt.handler/app
+             :css-dirs     ["resources/public/css"]}
 
   :profiles {:dev {:dependencies  [[binaryage/devtools "1.0.0"]
                                    [figwheel-sidecar "0.5.20"]]
