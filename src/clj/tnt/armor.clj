9d13653c9fe44c5be1dbb49623296863b06c7ada
John Maruska <john.maruska@rate.com>
2020-08-07 13:32:24 -0500

    we dont need a resource-root rn

--- src/clj/tnt/armor.clj
+++ src/clj/tnt/armor.clj
@@ -2,8 +2,6 @@
   (:require
    [tnt.util.resources :as resources]))
 
-(def resource-root "./resources/dungeons_and_dragons/armor")
-
 (def all (resources/load-directory "dungeons_and_dragons/armor"))
 
 ;; TODO: group-by paths, e.g. [:armor :light :name]
